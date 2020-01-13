24de7d3417b0a9b9fd3fa827fc0aa301d9f0480c
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-13 01:43:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+class CashRegister
+  attr_accessor :discount, :total,
+  
+  
+  
+  def initialize(discount = 0)
+    @discount = discount 
+    @total = 0 
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    
+    @items << title 
+  end 
+  
+  def items 
+    @items 
+  end 
+  
+end 
 
